113c5b4324f5d5d579d9395fa73bd96de9055425
Rahul8826 <77148168+Rahul8826@users.noreply.github.com>
2021-09-17 23:11:53 +0530

    textutils
    rahultext

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
-import About from './components/About';
+//import About from './components/About';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+//import {
+//  BrowserRouter as Router,
+//  Switch,
+//  Route,
+//  Link
+//} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light'); //whether dark mode is enabled or not
@@ -48,20 +48,20 @@ function App() {
   return (
    <>
 {/*<Navbar title="Textutils" aboutText="About Utils"/>*/}  
-<Router>
+{/*<Router>*/}
 <Navbar title="Textutils" mode={mode} toggleMode={toggleMode}/>
 <Alert alert={alert}/>
 <div className="container my-3">
-<Switch>
-          <Route path="/about">
+{/*<Switch>*/}
+          {/*<Route path="/about">
             <About />
-          </Route>
-          <Route path="/">
+  </?Route>*/}
+          {/*<Route path="/">*/}
           <Textform showAlert ={showAlert} heading="Enter your thoughts" mode={mode}/>
-          </Route>
-        </Switch>
+          {/*</Route>*/}
+        {/*</Switch>*/}
 </div>
-</Router>
+{/*</Router>*/}
 </>
   );
 }
